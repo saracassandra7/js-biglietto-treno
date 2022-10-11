4bc7173771de3bcc471eaa1e14df10ee731068bf
saracassandra7 <saracassandra7@gmail.com>
2022-10-11 17:37:36 +0200

    [start] exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('ciao');
+const kmPercorso = parseInt(prompt('Quanti km devi percorrere per il tuo viaggio?'));
+const etaUtente = parseInt(prompt('Quanti anni hai?'));
+
+const prezzoAlKm = 0.21
+const maggiorenne = 18
+const over = 65
+const scontoMaggiorenne = 0.2
+const scontoOver = 0.4
+
+const PrezzoBiglietto = kmPercorso * prezzoAlKm
+let prezzoTot;
+
+if (etaUtente < maggiorenne){
+  prezzoTot = PrezzoBiglietto * scontoMaggiorenne;
+  console.log(PrezzoBiglietto);
+
+} else if (etaUtente > over){
+  prezzoTot = PrezzoBiglietto * scontoOver;
+  console.log(PrezzoBiglietto);
+  
+} else {
+  prezzoTot = PrezzoBiglietto;
+  console.log(PrezzoBiglietto);
+  
+}
+
